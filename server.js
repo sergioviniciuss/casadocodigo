503d2c597a821924ad599a736b0e3af3f6a94950
Sérgio Lucena <sergioviniciuss@gmail.com>
2018-12-03 23:39:50 +0100

    Refactors(server): Moves express logic to custom-express

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const app = express();
+const app = require('./src/config/custom-express');
 
 app.listen(3000, () => {
   console.log(`listening on port 3000...`);

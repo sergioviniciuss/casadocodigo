993f8bd361c49338b04c2c5536880abaabfe8c59
Sérgio Lucena <sergioviniciuss@gmail.com>
2019-01-10 21:27:49 +0100

    Feat(Remove): Adds middleware to allow to call static files in the application

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+app.use('/static', express.static('src/app/public'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 

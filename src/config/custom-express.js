45070112bcf68d599dc30d08fbd2eb0739e54689
Sérgio Lucena <sergioviniciuss@gmail.com>
2019-01-10 22:36:31 +0100

    Feat(Edit): Adds method-override needed for applying a middleware to override the POST method with a PUT

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 app.use('/static', express.static('src/app/public'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
 app.use(express.json());
 
 const routes = require('../app/routes/routes');
